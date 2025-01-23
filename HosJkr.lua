0b67216c954e6ac94b8713cf7618c8fabfea6d73
Marina <135920061+marinaposting@users.noreply.github.com>
2025-01-23 11:35:43 -0300

    Add files via upload

---
+++ HosJkr.lua
@@ -0,0 +1,41 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Hosses22 Jokers Malverk
+--- MOD_ID: HosJkrMlvr
+--- PREFIX: hjkm
+--- MOD_AUTHOR: [MARINA]
+--- MOD_DESCRIPTION: turns the 5 legendary jokers into cute drawings by hosses22
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+AltTexture{
+    key = 'jokers-hosses',
+    set = 'Joker',
+    path = 'jokers-hosses.png',
+    keys = {
+        'j_caino',
+        'j_triboulet',
+        'j_yorick',
+        'j_chicot',
+        'j_perkeo'
+    },
+    soul_keys = {
+        'j_caino',
+        'j_triboulet',
+        'j_yorick',
+        'j_chicot',
+        'j_perkeo'
+    },
+}
+
+TexturePack{
+    key = 'jokers-hosses',
+    textures = {
+        'malverk_jokers-hosses_jokers-hosses'
+    }
+}
+----------------------------------------------
+------------MOD CODE END----------------------
---
+++ README.md
@@ -0,0 +1,4 @@
+# Hosses21 Jokers
+[Malverk](https://github.com/Eremel/Malverk) texture mod for Balatro with artwork by Hosses21.
+
+Requires [Malverk](https://github.com/Eremel/Malverk).
